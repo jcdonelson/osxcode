745d24d218d92ad8d8046bf941c429d328142b48
JimD <git@jimdonelson.com>
2014-05-31 19:02:06 -0400

    Initial Commit

---
+++ prog1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  prog1
+//
+//  Created by james donelson on 5/31/14.
+//  Copyright (c) 2014 james donelson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
